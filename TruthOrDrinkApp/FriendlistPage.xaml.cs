796402316e848c457974e630bdc49d300d033fa1
Kasper Bakker <2305440bakker@zuyd.nl>
2024-12-03 12:56:17 +0100

    Vriendenlijst en meerdere knoppen aangepast

--- TruthOrDrinkApp/FriendlistPage.xaml.cs
+++ TruthOrDrinkApp/FriendlistPage.xaml.cs
@@ -1,9 +1,59 @@
-namespace TruthOrDrinkApp;
+using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+using System.Linq;
 
-public partial class FriendlistPage : ContentPage
+namespace TruthOrDrinkApp
 {
-	public FriendlistPage()
+	public partial class FriendlistPage : ContentPage
 	{
-		InitializeComponent();
+		// Lijsten voor vrienden
+		private ObservableCollection<string> Friends { get; set; }
+		public ObservableCollection<string> FilteredFriends { get; set; }
+
+		public FriendlistPage()
+		{
+			InitializeComponent();
+
+			// Voorbeeldvrienden
+			Friends = new ObservableCollection<string>
+			{
+				"Alice" ,
+				"Bob",
+				"Charlie",
+				"David",
+				"Eve"
+			};
+
+			// Gefilterde vriendenlijst
+			FilteredFriends = new ObservableCollection<string>(Friends);
+
+			// Koppel aan de BindingContext
+			BindingContext = this;
+		}
+
+		// Zoekfunctie
+		private void OnSearchTextChanged(object sender, TextChangedEventArgs e)
+		{
+			var searchText = e.NewTextValue.ToLower();
+			FilteredFriends.Clear();
+
+			foreach (var friend in Friends.Where(f => f.ToLower().Contains(searchText)))
+			{
+				FilteredFriends.Add(friend);
+			}
+		}
+
+		// Vriend toevoegen
+		private async void OnAddFriendClicked(object sender, EventArgs e)
+		{
+			// Open een prompt om een nieuwe vriend toe te voegen
+			string result = await DisplayPromptAsync("Voeg vriend toe", "Voer naam van vriend in:");
+
+			if (!string.IsNullOrWhiteSpace(result))
+			{
+				Friends.Add(result);
+				FilteredFriends.Add(result);
+			}
+		}
 	}
 }
